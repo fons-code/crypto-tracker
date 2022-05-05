6013ddf89935d6efb18ff736f549353d01a73f2c
Alessandri Fonseca <fons.code@gmail.com>
2022-05-04 21:55:39 -0400

    💩 Add: basic sign up feature

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,17 @@
 import React, {createContext} from 'react'
-
+import {auth} from '../firebase.config'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
 interface Props {
     children: React.ReactChild
 }
 
 export const AuthContext = createContext<any>(null)
 
+const signup = (email : string, password : string) => {
+    return createUserWithEmailAndPassword(auth,email,password)
+}
+
 export const AuthProvider = ({children} : Props) => 
-    <AuthContext.Provider value={{login: true}}>
+    <AuthContext.Provider value={signup}>
         {children}
     </AuthContext.Provider>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,20 +1,42 @@
 import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'        
 import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 import Input from '../components/Input'
 
 export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [repeatPassword, setRepeatPassword] = useState('')
+  const [error, setError] = useState('')
+  const signup = useAuth()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setError('')
+    e.preventDefault()
+    if(password == repeatPassword){
+    try {
+      await signup(email, password)
+      navigate('/')
+    } catch (error : any) {
+      console.log(error)
+      setError(error.code)
+    }
+    }else{
+      setError('Passwords do not match')
+    }
+  }
   return (
     <>
       <h1>Sign up</h1>
-      <form>
+      <form onSubmit={e => handleSubmit(e)}>
         <Input name='email' state={email} setState={setEmail} type="email" />
         <Input name='password' state={password} setState={setPassword} type="password" />
         <Input name='Repeat password' state={repeatPassword} setState={setRepeatPassword} />
         <button>Sign up</button>
       </form>
+      {error && <p>{error}</p>}
       <Link to="/login">
         Do you already have an account?
       </Link>
