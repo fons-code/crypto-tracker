0ff1fe80a26513c3783e4c82334d914d67a6e936
Alessandri Fonseca <fonscode@MacBook-Air-de-Alessandri.local>
2022-05-17 22:10:04 -0400

    Change main content to chakra ui

--- src/components/Datatable/index.tsx
+++ src/components/Datatable/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import {formatNumber} from '../../utils/formatNumber'
 import {Coin} from '../../types/Coin'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 import './styles.css'
 
 interface Props {
@@ -12,38 +23,38 @@ const colorizeNumber = (num : number) => num > 0 ? 'success' : 'danger'
 
 const Datatable : React.FC<Props> = ({data}) => {
   return (
-    <div className='table_container'>
-    <table>
-        <thead>
-          <tr>
-            <th scope='col'>#</th>
-            <th scope='col'>Coin</th>
-            <th scope='col'>Price</th>
-            <th scope='col'>24h</th>
-            <th scope='col'>7d</th>
-            <th scope='col'>30d</th>
-            <th scope='col'>Mkt cap</th>
-          </tr>
-        </thead>
-        <tbody>
+    <TableContainer>
+    <Table>
+        <Thead>
+          <Tr>
+            <Th scope='col'>#</Th>
+            <Th scope='col'>Coin</Th>
+            <Th scope='col'>Price</Th>
+            <Th scope='col'>24h</Th>
+            <Th scope='col'>7d</Th>
+            <Th scope='col'>30d</Th>
+            <Th scope='col'>Mkt cap</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
             {data.map((coin, count) => {
               const {market_data,id,image,name, symbol} = coin 
               return(
-                <tr key={id}>
-                  <td scope='row'>{count + 1}</td>
-                  <td><img src={image.thumb} alt="" /> {name} <span className='secondary'>({symbol})</span> </td>
-                  <td>${formatNumber(market_data.current_price.usd)}</td>
-                  <td className={colorizeNumber(market_data.price_change_percentage_24h)}>{formatNumber(market_data.price_change_percentage_24h)}</td>
-                  <td className={colorizeNumber(market_data.price_change_percentage_7d)}>{formatNumber(market_data.price_change_percentage_7d)}</td>
-                  <td className={colorizeNumber(market_data.price_change_percentage_30d)}>{formatNumber(market_data.price_change_percentage_30d)}</td>
-                  <td>${market_data.market_cap.usd}</td>
-                </tr>
+                <Tr key={id}>
+                  <Td scope='row'>{count + 1}</Td>
+                  <Td><img src={image.thumb} alt="" /> {name} <span className='secondary'>({symbol})</span> </Td>
+                  <Td>${formatNumber(market_data.current_price.usd)}</Td>
+                  <Td className={colorizeNumber(market_data.price_change_percentage_24h)}>{formatNumber(market_data.price_change_percentage_24h)}</Td>
+                  <Td className={colorizeNumber(market_data.price_change_percentage_7d)}>{formatNumber(market_data.price_change_percentage_7d)}</Td>
+                  <Td className={colorizeNumber(market_data.price_change_percentage_30d)}>{formatNumber(market_data.price_change_percentage_30d)}</Td>
+                  <Td>${market_data.market_cap.usd}</Td>
+                </Tr>
               )
             })
             }
-        </tbody>
-      </table>
-    </div>
+        </Tbody>
+      </Table>
+    </TableContainer>
   )
 }
 export default Datatable
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,65 @@
-import React,{useState,useEffect} from 'react'
-import Input from '../components/Input'
-import Datatable from '../components/Datatable'
-import searchIcon from '../assets/search.svg'
-import {useAuth} from '../hooks/useAuth'
-
-import { Coin } from '../types/Coin'
+import React, { useState, useEffect } from "react";
+import { Input } from "@chakra-ui/react";
+import Datatable from "../components/Datatable";
+import { Search2Icon } from "@chakra-ui/icons";
+import { useAuth } from "../hooks/useAuth";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  InputGroup,
+  InputRightElement,
+  VStack
+} from "@chakra-ui/react";
+import { Coin } from "../types/Coin";
 
 export default function Home() {
-  const [cryptoData, setCryptoData] = useState<Coin[] | []>([])
-  const [search, setSearch] = useState<string>("")
+  const [cryptoData, setCryptoData] = useState<Coin[] | []>([]);
+  const [search, setSearch] = useState<string>("");
 
-  const [filteredCoins, setFilteredCoins] = useState<Coin[] | []>([])
+  const [filteredCoins, setFilteredCoins] = useState<Coin[] | []>([]);
 
   const searchCoins = (name: string) => {
-    setSearch(name)
-    setFilteredCoins(cryptoData.filter(crypto => crypto.name.toLowerCase().includes(name.toLowerCase().trim())))
-  }
+    setSearch(name);
+    setFilteredCoins(
+      cryptoData.filter((crypto) =>
+        crypto.name.toLowerCase().includes(name.toLowerCase().trim())
+      )
+    );
+  };
 
   const getData = async () => {
-    const res = await fetch('https://api.coingecko.com/api/v3/coins/')
-    const data = await res.json()
-    setCryptoData(data)
-    setFilteredCoins(data)
-  }
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/");
+    const data = await res.json();
+    setCryptoData(data);
+    setFilteredCoins(data);
+  };
   useEffect(() => {
-    getData()
-  }, [])
-        console.log(useAuth())
+    getData();
+  }, []);
+  console.log(useAuth());
   return (
-    <div>
-        <h1 className='sr-only'>Crypto tracker</h1>
-        <div>
-          <Input name='search' state={search} setState={searchCoins} icon={searchIcon}/>
-          <Datatable data={filteredCoins} />
-        </div>
-    </div>
-  )
+    <VStack>
+      <h1 className="sr-only">Crypto tracker</h1>
+      <div>
+        <Box maxW='350px'>
+        <FormControl>
+          <FormLabel htmlFor="search">Search</FormLabel>
+          <InputGroup>
+            <InputRightElement children={<Search2Icon />} />
+            <Input
+              type="text"
+              id="search"
+              placeholder="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </InputGroup>
+        </FormControl>
+        </Box>
+        <Datatable data={filteredCoins} />
+      </div>
+    </VStack>
+  );
 }
+
