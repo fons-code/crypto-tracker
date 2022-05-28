e5da9c1f07563fe728cf2104dcd63939fa8aea53
Alessandri Fonseca <fonscode@AirdeAlessandri.lan>
2022-05-27 21:53:46 -0400

    Refactor login and sign up forms to chakra

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,52 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 //components
-import Input from '../components/Input'
+import { Heading, VStack, Container, Button } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+} from "@chakra-ui/react";
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <>
-      <h1>Login</h1>
-      <form>
-        <Input name='email' state={email} setState={setEmail} type="email"/>      
-        <Input name='password' state={password} setState={setPassword} type="password"/>
-      </form>
-      <Link to='/signup'>
-        Create a new account
-      </Link>
-    </>
-  )
+    <Container>
+      <VStack spacing={4}>
+        <Heading as={"h2"} size="xl">
+          Login
+        </Heading>
+          <FormControl isRequired>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <FormHelperText>We'll never share your email.</FormHelperText>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </FormControl>
+          <Button variant="solid" colorScheme="cyan">
+            Login
+          </Button>
+        <Link to="/signup">Create a new account</Link>
+      </VStack>
+    </Container>
+  );
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,46 +1,74 @@
-import React, { useState } from 'react'
-import {useNavigate} from 'react-router-dom'        
-import {Button} from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-import { useAuth } from '../hooks/useAuth'
-import Input from '../components/Input'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { Heading, Container, VStack } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+} from "@chakra-ui/react";
 
 export default function Signup() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [repeatPassword, setRepeatPassword] = useState('')
-  const [error, setError] = useState('')
-  const signup = useAuth()
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [error, setError] = useState("");
+  const signup = useAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    setError('')
-    e.preventDefault()
-    if(password == repeatPassword){
-    try {
-      await signup(email, password)
-      navigate('/')
-    } catch (error : any) {
-      console.log(error)
-      setError(error.code)
+    setError("");
+    e.preventDefault();
+    if (password == repeatPassword) {
+      try {
+        await signup(email, password);
+        navigate("/");
+      } catch (error:any) {
+        console.log(error);
+        setError(error.code);
+      }
+    } else {
+      setError("Passwords do not match");
     }
-    }else{
-      setError('Passwords do not match')
-    }
-  }
+  };
   return (
     <>
-      <h1>Sign up</h1>
-      <form onSubmit={e => handleSubmit(e)}>
-        <Input name='email' state={email} setState={setEmail} type="email" />
-        <Input name='password' state={password} setState={setPassword} type="password" />
-        <Input name='Repeat password' state={repeatPassword} setState={setRepeatPassword} />
-        <Button>Sign up</Button>
-      </form>
-      {error && <p>{error}</p>}
-      <Link to="/login">
-        Do you already have an account?
-      </Link>
+        <VStack spacing={4}>
+          <Heading size="xl">Sign up</Heading>
+          <form onSubmit={e => {handleSubmit(e)}}>
+          <FormControl isRequired>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <FormHelperText>We'll never share your email.</FormHelperText>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="email">Password</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </FormControl>
+          <Button>Sign up</Button>
+          {error && <p>{error}</p>}
+
+          </form>
+          <Link to="/login">Do you already have an account?</Link>
+        </VStack>
     </>
-  )
+  );
 }
